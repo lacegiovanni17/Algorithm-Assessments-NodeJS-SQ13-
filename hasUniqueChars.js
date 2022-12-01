07c9e595898317048e0dd1c140f1e64657c5423c
Chidike Henry <30509335+lacegiovanni17@users.noreply.github.com>
2022-12-02 00:01:13 +0100

    Add files via upload

---
+++ hasUniqueChars.js
@@ -0,0 +1,28 @@
+//Write a program to determine if a string contains all unique characters. Return true if it does and false otherwise. The string
+//may contain any of the 128 ASCII characters. SPECIFICATION. Parameters - str: String - The string that may or may not contain all
+//unique characters. Return Value. Boolean - True if all characters inn the string are unique.
+//Examples:
+//   str                         Return Value
+//   "abcdefg"                     true
+//  "abbcdefg"                     false
+
+//Solution 1:
+
+const hasUniqueChars = (s) => {
+  let tempAr = [];
+  let alreadyInTempArr = [];
+  let spiltArr = s.split(" ");
+  for (let i = 0; i < spiltArr.length; i++) {
+    if (!alreadyInTempArr.includes(splitArr[i])) {
+      alreadyInTempArr.push(splitArr[i]);
+    } else {
+      alreadyInTempArr.push(splitArr[i]);
+    }
+  }
+  if (alreadyInTempArr.length > 0) {
+    return false;
+  }
+  return true;
+};
+
+
