4b366844efd30c99085ca00a1b422facb2c64b8c
Chidike Henry <dk1online25@gmail.com>
2022-12-02 00:40:40 +0100

    new feat

--- withdraw.js
+++ withdraw.js
@@ -1,4 +1,5 @@
-// An ATM only has 100, 50 and 20 dollar bills (USD) available to be dispensed.// Given an amount between 40 and 10000 dollars (inclusive) and
+// An ATM only has 100, 50 and 20 dollar bills (USD) available to be dispensed.
+// Given an amount between 40 and 10000 dollars (inclusive) and
 //assuming that the ATM wants to use as few bills as possible, determinate the minimal number of 100, 50 and 20 dollar bills
 //the ATM needs to dispense(in that order).// Here is the specification for the withdraw method you'll complete:
 //// withdraw(amount)
