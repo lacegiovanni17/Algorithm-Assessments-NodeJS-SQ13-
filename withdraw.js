0e29027e4331ead80a9443c24634964bb516fc59
Chidike Henry <dk1online25@gmail.com>
2022-12-02 22:44:28 +0100

    updated new feat
    solution to Tuesday assessment

--- withdraw.js
+++ withdraw.js
@@ -20,3 +20,29 @@
 ///** * Determines the minimal number of 100, 50 and 20 dollar bills to dispense.
 //* * @param { number } amount - The amount to dispense from the ATM. * @returns { number[] }
 //The counts of 100, 50, and 20 dollar bills respectively. * /
+
+function withdraw(amount) {
+    let result = [];
+    let hundred = 0;
+    let fifty = 0;
+    let twenty = 0;
+    while (amount >= 100) {
+        amount -= 100;
+        hundred++;
+    }
+    while (amount >= 50) {
+        amount -= 50;
+        fifty++;
+    }
+    while (amount >= 20) {
+        amount -= 20;
+        twenty++;
+    }
+    result.push(hundred, fifty, twenty);
+    return result;
+}
+
+console.log(withdraw(250));
+console.log(withdraw(260));
+console.log(withdraw(370));
+
